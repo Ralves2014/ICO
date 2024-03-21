b6c9b78ab247205b32af242e17e2602398951e31
Ralves2014 <rbelchiorinho@gmail.com>
2024-03-21 11:34:37 +0000

    project update

--- Projetos/1ª Projeto/optimization_line_search_methods.m
+++ Projetos/1ª Projeto/optimization_line_search_methods.m
@@ -1,4 +1,4 @@
-function proposta1_line_search_methods()
+function optimization_line_search_methods()
     % NESTE m.FILE IREMOS:
     % - EXPLORAR OS MÉTODOS DE BUSCA EM LINHA MENCIONADOS(MÉTODO DE DESCIDA
     %   MAXIMA, MÉTODO DE NEWTON-RAPHSON E O MÉTODO DE DESCIDA MAXIMA COM
@@ -10,13 +10,16 @@ function proposta1_line_search_methods()
 
     % ATRIBUIÇAO DE VALORES A VARIAVEIS COMUNS
     errodf = 15e-2;
-    Nmax = randi(35,50);
-    NPontosIniciais = randi(8,13); 
+    Nmax = getNmax()
+    NPontosIniciais = getNPontosIniciais() 
     a = [-11;-11]; b = [7;7];
     Nvar=2;
 
     % LISTA DE FUNÇOES OBJETIVO QUE VAO SER TESTADAS
-    
+    r = @(x) 100*(x(2)-x(1)^2)^2 + (1-x(1))^2;
+    g = @(x) x(1)*(x(1) + 3)*(x(1) - 3)*(x(1) - 5) + x(2)*(x(2) + 3)*(x(2) - 3)*(x(2) - 5);
+    f = @(x) ((x(1))^2 - 100)*((x(1))^2 - 61) + ((x(2))^2 - 25)*((x(2))^2 - 16) - 15*cos(x(1)-(7/5))*cos(x(2)-(9/2));
+
     % Supostamente tens de ter o gradiente calculado
     % Chamas cada um dos metodos
 
@@ -32,6 +35,14 @@ function proposta1_line_search_methods()
     
     
 
+end
+
+function N = getNmax()
+    N = randi([35,50]);
+end
+
+function Npi = getNPontosIniciais()
+    Npi = randi([8,13]);
 end
 
 function [dfx1,dfx2] = calcular_derivada(of)
