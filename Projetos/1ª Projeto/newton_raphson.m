fa6be632ff343eb0d0658e46b57b1dfbc815bf33
gbotelho7 <gmida@iscte-iul.pt>
2024-03-25 22:32:55 +0000

    rand Init Points + Wolfe

--- Projetos/1ª Projeto/newton_raphson.m
+++ Projetos/1ª Projeto/newton_raphson.m
@@ -1,9 +1,9 @@
-function [Lista, LNit, Lopt] = newton_raphson(f, Nvar, Nmax, errodf, NPontosIniciais, x_values, y_values)
+function [Lista, LNit, Lopt] = newton_raphson(f, Nvar, Nmax, errodf, NPontosIniciais, x_values, y_values, pontos_iniciais)
     clc;
 
     syms a b 'real'
 
-    v = [a; b]; 
+    v=[a;b];
         
     S = f(v);
 
@@ -20,19 +20,19 @@ function [Lista, LNit, Lopt] = newton_raphson(f, Nvar, Nmax, errodf, NPontosInic
     LNit = [];
     Lopt = [];
 
-    a = x_values;
-    b = y_values;
 
     for i = 1:NPontosIniciais
-        x = (b - a) .* rand(Nvar, 1) + a;
+        x = pontos_iniciais(:, i);
         Lista = [Lista, x];   
         dfx = df(x);   
         N = 1;     
 
         while norm(dfx) > errodf && N < Nmax  
             d = -inv(d2f(x)) * dfx;
-
-            lambda = Wolfe(x, f, df, d, 0.1); % Chama a função Wolfe para calcular lambda. beta0 pode ser qualquer valor, pois ao contrario de sdm o passo não e calculado iterativamente
+            beta0 = 1; % No método de Newton-Raphson, pode ser simplesmente 1
+            
+            lambda = Wolfe(x, f, df, d, beta0); % Calculando o passo lambda
+            
             x = x + lambda * d;  
             Lista = [Lista, x];       
             dfx = df(x);
@@ -42,4 +42,52 @@ function [Lista, LNit, Lopt] = newton_raphson(f, Nvar, Nmax, errodf, NPontosInic
         Lopt = [Lopt x];
         LNit = [LNit N];
     end 
+
+    % PARTE 7: OBTENÇÃO DO MÍNIMO GLOBAL
+
+    xopt=Lopt(:,1); 
+    
+    for i=2:NPontosIniciais
+        if f(xopt)>f(Lopt(:,i)); 
+            xopt=Lopt(:,i);      
+        end
+    end
+
+    % PARTE 8: DEFINIÇÃO E EXIBIÇÃO DOS OUTPUTS (SÃO EXIBIDOS NA COMMAND
+    % WINDOW PORQUE NÃO É COLOCADO ";" NO FINAL, EMBORA SEM LEGENDA)(PARA
+    % OBTER LEGENDA COLOCAR PARTE 9 DO FICHEIRO "descidaMaximaV1aula.m")
+    
+    xoptimo=xopt        
+    foptimo=f(xopt)    
+    dffinal=df(xopt)    
+    NIterMean=mean(LNit)
+
+
+    disp(xoptimo);                    % EXIBIÇÃO DO (OUTPUT) PTO ÓPTIMO xoptimo
+    disp('xoptimo:');                 % EXIBIR PTO ÓPTIMO LEGENDADO 'xoptimo'
+    
+    disp(foptimo);                  % EXIBIÇÃO DO (OUTPUT) VALOR ÓPTIMO foptimo
+    disp('foptimo:');               % EXIBIR VALOR ÓPTIMO LEGENDADA 'foptimo'         
+    
+    disp(dffinal);       % EXIBIÇÃO DO (OUTPUT) GRADIENTE NO PTO ÓPTIMO dffinal
+    disp('dffinal:');    % EXIBIR GRADIENTE NO PTO ÓPTIMO LEGENDADO 'dffinal'       
+    
+    disp('NIterMean:');  % EXIBIR Nº MÉDIO DE ITERAÇ. LEGENDADO 'NIterMean'
+    disp(NIterMean);     % EXIBIÇÃO DO (OUTPUT) Nº MÉDIO DE ITERAÇÕES NIterMean
+    
+
+
+    % PARTE 9: EXIBIÇÃO DOS GRÁFICOS COM RECURSO A OUTROS m.FILES
+    
+    close all
+
+    nivelGraficos(f,x_values(1),y_values(1),x_values(2),y_values(2))
+
+    hold on
+
+    graficosLineSearch(Lista,LNit,Lopt)
+
+    hold off
+
+
 end
