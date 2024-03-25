0588b94b55643470ae9be2a5b0ee9a5d46f1cabc
Miguel Valadares <mlvss@iscte-iul.pt>
2024-03-25 12:45:22 +0000

    teste - não utilizar

---
+++ Projetos/1ª Projeto/MLV/getNPontosIniciais.m
@@ -0,0 +1,7 @@
+function List=getNPontosIniciais(Np, Nvar,x_values,y_values)
+    a=x_values; b=y_values; 
+    List = [];
+    for i = 1: Np
+        x=(b-a).*rand(Nvar,1)+a; 
+        List=[List, x];
+    end
---
+++ Projetos/1ª Projeto/MLV/steepest_descent_static_2.m
@@ -0,0 +1,40 @@
+function [Lista, LNit, Lopt] = steepest_descent_static_2(f, Nmax, errodf, PontosIniciais, x_values, y_values, lambda)
+clc;
+
+syms a b 'real'
+
+v=[a; b]; 
+        
+S=f(v);
+
+dS=jacobian(S,v); 
+                    
+df=@(x) double(subs(dS,v,x)');
+                           
+Lista=[];
+LNit=[];
+Lopt=[];
+
+a=x_values; b=y_values;
+
+[~, num_points] = size(PontosIniciais);
+
+
+for i=1:num_points
+    x = PontosIniciais(:,i);
+    Lista=[Lista, x];
+    dfx=df(x);           
+    N=1;     
+    
+    while norm(dfx)>errodf && N<Nmax  
+        d = -dfx;
+        x=x+lambda*d;  
+        Lista=[Lista, x];       
+        dfx=df(x);  
+        N=N+1;           
+    end 
+    
+    Lopt=[Lopt x];
+    LNit=[LNit N];
+end
+
